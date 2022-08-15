33af5a150231f8629c3897c6310ce26034c8c8b4
Brownoak <helanii93@gmail.com>
2022-08-15 14:56:58 -0700

    adding js for modal

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+var main = document.getElementsByTagName("HEAD")[0];
+
+// Create new link Element
+var link = document.createElement("link");
+
+// set the attributes for link element
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "link.css";
+
+document.querySelector(".event_img").addEventListener("click", function () {
+  document.querySelector(".popup").classList.add("active");
+  main.appendChild(link);
+});
+
+document.querySelector(".popup .close_btn")
+  .addEventListener("click", function (e) {
+    document.querySelector(".popup").classList.remove("active");
+   main.removeChild(link);
+},
+);
