f4f1bc5ffb09283d3ca352ae0fa9562cad6ab05a
Brownoak <helanii93@gmail.com>
2022-08-15 17:57:51 -0700

    adding a js to display the modal

--- js/modal.js
+++ js/modal.js
@@ -15,7 +15,74 @@ document.querySelector(".event_img").addEventListener("click", function () {
 
 });
 
-document.querySelector(".popup .close_btn")
+
+
+document.querySelector(".event_img2").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+
+document.querySelector(".event_img3").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+document.querySelector(".event_img4").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+document.querySelector(".event_img5").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+document.querySelector(".event_img6").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+});
+
+
+document.querySelector(".event_img7").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+document.querySelector(".event_img8").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+  document.querySelector(".event_img9").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+  document.querySelector(".event_img10").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+    main.appendChild(link);
+    nav.classList.remove("black_nav");
+  
+  });
+
+  document.querySelector(".popup .close_btn")
   .addEventListener("click", function (e) {
     document.querySelector(".popup").classList.remove("active");
    main.removeChild(link);
