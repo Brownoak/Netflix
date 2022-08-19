b4ca2dcfebcf58efa718a172ca5e6567815d9da9
Brownoak <helanii93@gmail.com>
2022-08-19 11:36:01 -0700

    creating the variable for the api

--- js/api.js
+++ js/api.js
@@ -1,9 +1,13 @@
-const nav = document.getElementById("nav");
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 100) {
-    nav.classList.add("black_nav");
-  } else {
-    nav.classList.remove("black_nav");
-  }
-});
+const api = config.API_KEY;
+const base_url= config.BASE_URL;
+const final_url = base_url + config.MID_URL + api;
+const img_url = config.IMG_URL;
+
+const action = config.FETCH_ACTION_MOVIES;
+const tvshows= config.FETCH_TV_SHOWS;
+const comedy = config.FETCH_COMEDIES;
+const crime = config.FETCH_CRIME_MOVIES;
+const romance = config.FETCH_ROMANTIC_MOVIES;
+const horror = config.FETCH_HORROR_MOVIES;
+const scienceFic = config.FETCH_SCIENCE_FIC_MOVIES;
 
