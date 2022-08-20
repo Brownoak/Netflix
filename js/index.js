661ebdde955ef94bcc575c10176acda126830802
Brownoak <helanii93@gmail.com>
2022-08-19 19:15:28 -0700

    linking to the link.css

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+var main = document.getElementsByTagName("HEAD")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "css/link.css";
+
 const nav = document.getElementById("nav");
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 100) {
