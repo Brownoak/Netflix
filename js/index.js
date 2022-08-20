c5f04198f5e854ebcbe5517bd9a1b91b3454eed5
Brownoak <helanii93@gmail.com>
2022-08-19 19:04:38 -0700

    adding the black navigation when scrolled

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const nav = document.getElementById("nav");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    nav.classList.add("black_nav");
+  } else {
+    nav.classList.remove("black_nav");
+  }
+});
