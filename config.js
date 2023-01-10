be326ca13da6d13533ed175667d967003b2c1da3
Brownoak <helanii93@gmail.com>
2023-01-10 15:45:18 -0800

    adding config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+var config ={
+    API_KEY : '99aa75c4d8eed3c4071db48398bca33b',
+    BASE_URL : 'https://api.themoviedb.org/3',
+    IMG_URL : 'https://image.tmdb.org/t/p/original',
+    MID_URL : '/discover/movie?sort_by=popularity.desc&api_key=',
+    
+    FETCH_ACTION_MOVIES: '/discover/movie?api_key=99aa75c4d8eed3c4071db48398bca33b&with_networks=300',
+    FETCH_TV_SHOWS: '/discover/tv?api_key=99aa75c4d8eed3c4071db48398bca33b&with_genres=18',
+    FETCH_COMEDIES: '/discover/movie?api_key=99aa75c4d8eed3c4071db48398bca33b&with_genres=35',
+    FETCH_CRIME_MOVIES: '/discover/movie?api_key=99aa75c4d8eed3c4071db48398bca33b&with_genres=80',
+    FETCH_ROMANTIC_MOVIES : '/discover/movie?api_key=99aa75c4d8eed3c4071db48398bca33b&with_genres=10749',
+    FETCH_HORROR_MOVIES: '/discover/movie?api_key=99aa75c4d8eed3c4071db48398bca33b&with_genres=27',
+    FETCH_SCIENCE_FIC_MOVIES: '/discover/movie?api_key=99aa75c4d8eed3c4071db48398bca33b&with_genres=878',
+    FETCH_CHANGING_BANNER: '/movie/now_playing?api_key=99aa75c4d8eed3c4071db48398bca33b',
+}
+
